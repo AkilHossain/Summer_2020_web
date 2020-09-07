8c989d7e129b6950755dd8ea3e49af40abe11f11
AkilHossain <67995471+AkilHossain@users.noreply.github.com>
2020-09-07 12:05:24 +0600

    12.05 2nd

---
+++ Final_Lab_Exam/script.js
@@ -0,0 +1,40 @@
+"use strict"
+//a = 10;
+var b =30;
+let c =40;
+const e = 50;
+
+//console.log('test');
+//alert('test');
+//document.write('hfbjh');
+
+function f1(){
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('username').value;
+
+	var error = false;
+	if(username == ""){
+		document.getElementById('userMsg').innerHTML = "Username Can't left empty!";
+		error = true;
+	}else if(password == ""){
+		document.getElementById('passMsg').innerHTML = "password Can't left empty!";
+		error =true;
+	}
+
+	if(error){
+		return false;
+	}else{
+		return true;
+	}
+
+}
+
+var f2 = function(){
+	var username = document.getElementById('username').value;
+	if(username != ""){
+		document.getElementById('userMsg').innerHTML = "";
+		var password = document.getElementById('password').value;
+	if(password != ""){
+		document.getElementById('passMsg').innerHTML = "";
+	}
+}
